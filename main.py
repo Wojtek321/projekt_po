f405fa593ebbd4d36b6481c9a53a8e6a1602ab4c
Dominik Gasowski <dominiggk@gmail.com>
2023-06-15 19:51:53 +0200

    edjf

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ while(True):
     match wybor:
         case 1:
             os.system('cls')
-            print("----Zarzadzanie firmami----")
+            print("----Zarzadzanie firmami----ffffffffff")
             print("1. Dodaj firme")
             print("2. Usun firme")
             print("3. Przegladaj firmy")
